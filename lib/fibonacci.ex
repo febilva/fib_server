2b7cd8c0647fce66eefc20d0ec7a96bec8e88a15
febil <febil.tvr@gmail.com>
2019-04-03 16:46:05 +0530

    get and find value from the state

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -34,16 +34,19 @@ defmodule Fibonacci do
     Agent.start_link(fn -> initial_value end, name: __MODULE__)
   end
 
+  def find(key) do
+    Agent.get(__MODULE__, fn state -> Map.has_key?(state, key) end)
+  end
+
   def update(key, value) do
     Agent.update(__MODULE__, fn state -> Map.put(state, key, value) end)
-    # Agent.update(agent, fn state -> Map.put(state, key, value) end)
+  end
+
+  def get(key) do
+    Agent.get(__MODULE__, fn state -> Map.get(state, key) end)
   end
 
   def value do
     Agent.get(__MODULE__, & &1)
-  end
-
-  def increment do
-    Agent.update(__MODULE__, &(&1 + 1))
   end
 end
