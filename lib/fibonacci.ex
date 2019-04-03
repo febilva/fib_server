844fa995dc11bc96606c7992d24d904e61301e83
febil <febil.tvr@gmail.com>
2019-04-03 16:23:55 +0530

    agent basic skelton is added

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -1,4 +1,6 @@
 defmodule Fibonacci do
+  use Agent
+
   @moduledoc """
   Documentation for Fibonacci.
   """
@@ -26,5 +28,17 @@ defmodule Fibonacci do
 
   def calculate(n) do
     calculate(n - 1) + calculate(n - 2)
+  end
+
+  def start_link(initial_value \\ 0) do
+    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  end
+
+  def value do
+    Agent.get(__MODULE__, & &1)
+  end
+
+  def increment do
+    Agent.update(__MODULE__, &(&1 + 1))
   end
 end
